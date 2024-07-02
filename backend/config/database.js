94c10713518261bbb481928ab17eb16e9d19849c
fay <wahomeimani@gmail.com>
2024-07-02 11:30:19 +0300

    innit

--- backend/config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './hospital_management.db', // Path to your SQLite database file
-});
-
-module.exports = sequelize;
